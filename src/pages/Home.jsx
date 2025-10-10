6f3bbf25512b9287e2607c0c78bb8f25071b1b55
Rachit0919 <rachit0919@gmail.com>
2025-10-10 10:35:54 +0530

    Comment out BeforeLoginComponent in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -69,7 +69,7 @@ function Home() {
       <section className="w-full max-w-7xl">
         <Container>
           <div className="relative">
-            <BeforeLoginComponent />
+            {/* <BeforeLoginComponent /> */}
           </div>
         </Container>
       </section>
