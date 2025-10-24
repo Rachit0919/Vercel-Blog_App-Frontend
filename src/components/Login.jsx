acdc8c9d9883f13720395412515da7d8925534f3
Rachit0919 <rachit0919@gmail.com>
2025-10-24 18:54:14 +0530

    dp change 34

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -92,7 +92,7 @@ function Login() {
         {/* Error message */}
         {error && (
           <p className="text-red-500 mt-6 text-center text-sm font-medium">
-            {error}
+            {error.message}
           </p>
         )}
 
